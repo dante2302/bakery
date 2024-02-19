41252445e9c91a3041a9207a6cd0ee6201e92022
Dante <guchoo923@gmail.com>
2024-02-19 21:39:24 +0200

    add controllers

---
+++ bakeryServer/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace bakeryServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController
+    {
+        [HttpGet]
+        public void GetOrders()
+        {
+            
+        }
+
+        [HttpPost]
+        public void PostOrder()
+        {
+
+        }
+    }
+}
