356f6395f7c26cc8707ae2ff129c65a5f8f425de
Dante <guchoo923@gmail.com>
2024-02-25 21:25:29 +0200

    create all crud service methods

--- bakeryServer/Services/Services/ToppingService.cs
+++ bakeryServer/Services/Services/ToppingService.cs
@@ -1,6 +1,43 @@
-﻿namespace bakeryServer.Services
+﻿using bakeryServer.Models;
+using bakeryServer.Services.Repositories;
+
+namespace bakeryServer.Services
 {
-    public class ToppingService
+    public class ToppingService(ToppingRepo repo)
     {
+        private readonly ToppingRepo _repo = repo;
+        public async Task<bool> Create(Topping topping)
+        {
+            return await _repo.Create(topping);
+        }
+
+        public async Task<Topping?> ReadOne(int id)
+        {
+            return await _repo.ReadOne(id);  
+        }
+
+        public async Task<IEnumerable<Topping?>> ReadAll()
+        {
+            return await _repo.ReadAll();
+        }
+
+        public async Task<bool> Update(Topping newTopping)
+        {
+            return await _repo.Update(newTopping);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            Topping toppingForDeletion= await ReadOne(id);
+
+            if (toppingForDeletion is null)
+            {
+                return false;
+            }
+
+            return await _repo.Delete(toppingForDeletion);
+        }
+
+
     }
 }
