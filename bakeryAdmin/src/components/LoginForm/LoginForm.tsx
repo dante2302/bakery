172f599eed002588c89cc9315863a5fd686fc190
dante <guchoo923@gmail.com>
2024-03-11 19:38:47 +0200

    Add labes inside input

--- bakeryAdmin/src/components/LoginForm/LoginForm.tsx
+++ bakeryAdmin/src/components/LoginForm/LoginForm.tsx
@@ -2,57 +2,60 @@ import { useState, ChangeEvent } from "react";
 import "./LoginForm.css";
 
 interface loginFormState{
-    username: string,
-    password: string
+  username: string,
+  password: string
 }
 
 interface formInput extends HTMLInputElement{
-    name: string,
-    value: any
+  name: string,
+  value: any
 }
 
 export default function LoginForm(){
 
-    const defaultFormState: loginFormState = {
-        "username" : "",
-        "password": ""
-    }    
+  const defaultFormState: loginFormState = {
+    "username" : "",
+    "password": ""
+  }    
 
-    const [formState, setFormState]  = useState<loginFormState>(defaultFormState);
+  const [formState, setFormState]  = useState<loginFormState>(defaultFormState);
 
-    function formInputHandler(e: ChangeEvent<formInput>){
-        setFormState((state: loginFormState) => (
-            {...state, [e.target.name]: e.target.value}
-        ));
-    }
+  function formInputHandler(e: ChangeEvent<formInput>){
+    setFormState((state: loginFormState) => (
+      {...state, [e.target.name]: e.target.value}
+    ));
+  }
 
-    async function submitHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>){
-        e.preventDefault();
-    }
+  async function submitHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>){
+    e.preventDefault();
+  }
 
-    return (
-        <form className="login-form">
-            <div className="input-container">
-                <label htmlFor="adminUsername">Потребителско име</label>
-                <input 
-                    type="text"
-                    id="adminUsername"
-                    name="username"
-                    value={formState.username}
-                    onChange={(e) => formInputHandler(e)}
-                />
-            </div>
-            <div className="input-container">
-                <label htmlFor="adminPassword">Парола</label>
-                <input
-                    type="password"
-                    id="adminPassword"
-                    name="password"
-                    value={formState.password}
-                    onChange={(e) => formInputHandler(e)}
-                />
-            </div>
-            <button onClick={(e) => submitHandler(e)}>Вход</button>
-        </form>
-    )
+  return (
+    <form className="login-form">
+      <div className="admin-top">
+        Admin
+      </div>
+      <div className="input-container">
+        {!formState.username && <label htmlFor="adminUsername">Потребителско име</label>}
+        <input 
+          type="text"
+          id="adminUsername"
+          name="username"
+          value={formState.username}
+          onChange={(e) => formInputHandler(e)}
+        />
+      </div>
+      <div className="input-container">
+        {!formState.password && <label htmlFor="adminUsername">Парола</label>}
+        <input
+          type="password"
+          id="adminPassword"
+          name="password"
+          value={formState.password}
+          onChange={(e) => formInputHandler(e)}
+        />
+      </div>
+      <button onClick={(e) => submitHandler(e)}>Вход</button>
+    </form>
+  )
 }
