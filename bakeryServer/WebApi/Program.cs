d2dfeecedbe00de4c7d4793f629424b6be23857b
dante <guchoo923@gmail.com>
2024-03-18 15:23:10 +0200

    add authentication packages

--- bakeryServer/WebApi/Program.cs
+++ bakeryServer/WebApi/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-
+builder.Services.AddAuthentication();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BakeryContext>(options =>
 {
