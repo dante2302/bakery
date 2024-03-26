a50c0f3d4242ebed7c1be528d074d1348dc95484
dante <guchoo923@gmail.com>
2024-03-26 21:25:02 +0200

    require non nullable properties, test authorization

--- bakeryServer/Models/Topping.cs
+++ bakeryServer/Models/Topping.cs
@@ -3,6 +3,6 @@
     public class Topping
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
     }
 }
--- bakeryServer/Models/User.cs
+++ bakeryServer/Models/User.cs
@@ -6,9 +6,9 @@ namespace bakeryServer.Models
     {
         [Key]
         public int Id { get; set; }
-        public string PhoneNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
         public string ?Email { get; set; }
     }
 }
