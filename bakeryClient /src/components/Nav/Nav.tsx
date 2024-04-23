d425b8fbc80bb74cd0ad5d71f853ea1bde501211
dante <guchoo923@gmail.com>
2024-04-23 20:15:05 +0300

    add template pages

---
+++ bakeryClient /src/components/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import s from "./styles/Nav.module.css";
+export default function Nav(){
+    return(
+        <nav>
+            <div className={s["upper-nav"]}>
+            </div>
+            <div className={s["lower-nav"]}>
+            </div>
+        </nav>
+    )
+}
