ca21ab21f73c5f6cd2ce9315ff850fc0c68f0aee
dante <guchoo923@gmail.com>
2024-04-24 22:16:07 +0300

    extract main services to its own component, add new candy svg, fully style main services component

--- bakeryClient /src/components/Main/Main.tsx
+++ bakeryClient /src/components/Main/Main.tsx
@@ -1,29 +1,10 @@
-import { Cake, Candy, Cookie } from "../SVGs"
 import "./Main.scss";
+import MainServices from "./MainServices/MainServices";
 
 export default function Main(){
     return(
         <main>
-            <div className="wrap">
-            <h1>Main Services We Provide</h1>
-            <div className="inner">
-                <div className="food-type-container">
-                    <Candy />
-                    <h2>Candies</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, alias.</p>
-                </div>
-                <div className="food-type-container">
-                    <Cake />
-                    <h2>Cakes</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, alias.</p>
-                </div>
-                <div className="food-type-container">
-                    <Cookie />
-                    <h2>Cookies</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, alias.</p>
-                </div>
-            </div>
-            </div>
+            <MainServices />
         </main>
     )
 }
