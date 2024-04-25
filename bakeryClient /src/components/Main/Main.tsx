d4243b9830d0db68f4aef92607221d887c30aa6b
dante <guchoo923@gmail.com>
2024-04-25 17:48:37 +0300

    refactor font weights to variable instead of map, create productshowcase component

--- bakeryClient /src/components/Main/Main.tsx
+++ bakeryClient /src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import "./Main.scss";
 import MainServices from "./MainServices/MainServices";
+import ProductShowcase from "./ProductShowcase/ProductShowcase";
 
 export default function Main(){
     return(
         <main>
+            <ProductShowcase />
             <MainServices />
         </main>
     )
