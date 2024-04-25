3b5a69c4a218e18a005df1674041ba7690c0a867
dante <guchoo923@gmail.com>
2024-04-25 16:37:52 +0300

    create and partially style footer

--- bakeryClient /src/components/Home/Home.tsx
+++ bakeryClient /src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 export default function Home(){
     return (
         <div>
             <Header />
             <Main />
+            <Footer />
         </div>
     )
 }
