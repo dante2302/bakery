fa4e6921a6615c93feec76067701026cca1f7bed
dante <guchoo923@gmail.com>
2024-05-08 17:44:16 +0300

    create entity service and touch up IEntityService, basically doing the same as with repos

--- bakeryServer/Services/Services/Interfaces/IEntityService.cs
+++ bakeryServer/Services/Services/Interfaces/IEntityService.cs
@@ -1,12 +1,15 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Linq.Expressions;
+using bakeryServer.Models;
 
 namespace Services;
 
-public interface IEntityService<T>
+public interface IEntityService<T> where T : class, IEntity
 {
     Task<T> Create(T type);
     Task<T> ReadOne(int id);
-    Task<IEnumerable<T>> ReadAll();
+    Task<T> ReadOneByCondition(Expression<Func<T, bool>> expression);
+    Task<List<T>> ReadAll();
+    Task<List<T>> ReadAllByCondition(Expression<Func<T, bool>> expression);
     Task Update(T type);
     Task Delete(int id);
 }
