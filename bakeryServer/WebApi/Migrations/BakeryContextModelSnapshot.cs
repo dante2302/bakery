66a4fadf7e7a43efb8f626d1b9a4826f7a754410
dante <guchoo923@gmail.com>
2024-05-09 14:01:50 +0300

    fix migration namespaces

--- bakeryServer/WebApi/Migrations/BakeryContextModelSnapshot.cs
+++ bakeryServer/WebApi/Migrations/BakeryContextModelSnapshot.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using bakeryServer.Data.DbContexts;
+using Data.DbContexts;
 
 #nullable disable
 
