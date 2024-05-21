e800320b69ad078c13e5b37011b30f8bcdf8e3e7
dante <guchoo923@gmail.com>
2024-05-21 10:41:03 +0300

    create contact page

--- bakeryClient/src/components/Contact/Contact.tsx
+++ bakeryClient/src/components/Contact/Contact.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react"
 import "./Contact.scss";
 import * as formService from "../../services/formService";
+import * as contactService from "../../services/contactService";
+import MessageBlock from "../Messages/MessageBlock";
 
-export default function Contact(){
-    interface ContactFormState extends formService.BaseFormState
-    {
-        name: string,
-        email: string,
-        message: string
-    }
-    const defaultFormState: ContactFormState  = 
+export interface ContactFormState extends formService.BaseFormState {
+    name: string,
+    email: string,
+    message: string
+}
+
+export default function Contact() {
+    const defaultFormState: ContactFormState =
     {
         name: "",
         email: "",
@@ -17,40 +19,60 @@ export default function Contact(){
     };
 
     const [formState, setFormState] = useState(defaultFormState);
+    const [requestSuccess, setRequestSuccess] = useState<boolean>();
+    const [showMessage, setShowMessage] = useState<boolean>();
 
-    function contactSubmitHandler(e: React.FormEvent<HTMLButtonElement>)
-    {
+    async function contactSubmitHandler(e: React.FormEvent<HTMLButtonElement>) {
         e.preventDefault();
+        const success = await contactService.sendMessage(formState);
+        setRequestSuccess(success);
+        setShowMessage(true);
+        setTimeout(() => {
+            setShowMessage(false);
+        }, 2000)
     }
+
     return (
         <div className="outer-contact-wrap">
             <h1>Свържи се с нас</h1>
             <div className="inner-contact">
                 <form>
                     <label htmlFor="name">Име</label>
-                    <input 
-                     type="text"
-                     name="name"
-                     id="name"
-                     value={formState.name}
-                     onChange={(e) => formService.changeHandler(setFormState, e)}
-                     />
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        value={formState.name}
+                        onChange={(e) => formService.changeHandler(setFormState, e)}
+                    />
+
                     <label htmlFor="email">Имейл</label>
-                    <input 
-                     type="email"
-                     name="email"
-                     id="email"
-                     value={formState.email}
-                     onChange={(e) => formService.changeHandler(setFormState, e)}
-                     />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={formState.email}
+                        onChange={(e) => formService.changeHandler(setFormState, e)}
+                    />
+
                     <label htmlFor="message">Съобщение</label>
-                    <textarea 
-                     name="message"
-                     id="message"
-                     value={formState.message}
-                     onChange={(e) => formService.changeHandler(setFormState, e)}
-                     />
-                     <button onClick={(e) => contactSubmitHandler(e)}>Изпрати</button>
+                    <textarea
+                        name="message"
+                        id="message"
+                        value={formState.message}
+                        onChange={(e) => formService.changeHandler(setFormState, e)}
+                    />
+
+                    <button onClick={(e) => contactSubmitHandler(e)}>Изпрати</button>
+                    {
+                        showMessage && 
+                        <MessageBlock 
+                            heading={requestSuccess ? "Success! " : "Something Went Wrong..."} 
+                            message={requestSuccess ? "Your Message was sent." : "An Error Happened"}
+                            isSuccess={requestSuccess}
+                            additionalStyles="contact-message-block"
+                        />
+                    }
                 </form>
                 <div>
                     <h6>Контакти</h6>
--- bakeryClient/src/services/contactService.ts
+++ bakeryClient/src/services/contactService.ts
@@ -1,4 +1,15 @@
-export function sendMessage()
-{
+import { ContactFormState } from "../components/Contact/Contact";
+const BASE_URL = "localhost:";
 
+export async function sendMessage(formState: ContactFormState)
+{
+    try{
+        await fetch(BASE_URL, {body: JSON.stringify(formState)});
+        return true;
+    }
+    catch(e)
+    {
+        console.log(e);
+        return false;
+    }
 }
